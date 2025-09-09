11b453ece5e00755a3ddc4f742f1c8c2306518c5
gronksol <arcanefi@tuta.io>
2025-09-09 16:59:31 +1200

    fix: re-export Q32x96

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ pub mod error;
 pub mod fixed_point;
 pub mod integers;
 
-pub use fixed_point::{SQ64x64, SQ1x62, SQ0x63, Q1x63, Q2x62, Q64x64, ShortQ64x64};
+pub use fixed_point::{SQ64x64, SQ1x62, SQ0x63, Q1x63, Q2x62, Q64x64, ShortQ64x64, Q32x96};
